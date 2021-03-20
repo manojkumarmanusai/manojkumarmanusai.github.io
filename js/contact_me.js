3c083a6cb5d4b413c41247b913e6cab6ebaf6022
manojkumarmanusai <manusai.manoj@gmail.com>
2021-03-20 20:07:35 +0530

    Adding files

---
+++ js/contact_me.js
@@ -0,0 +1,45 @@
+var form = document.getElementById("contactForm");
+    
+async function handleSubmit(event) {
+      event.preventDefault();
+      var email = document.getElementById("email").value;
+      if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)))
+      {
+      	alert("Please enter a valid email id");
+      	return;
+      }
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+      }).catch(error => {
+         // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+      });
+}
+
+function removeSpaces(string) {
+ return string.split(' ').join('');
+}
+form.addEventListener("submit", handleSubmit)
